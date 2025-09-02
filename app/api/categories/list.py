31d2e206bcb052ccddef7d71befd693567e7f5b3
ashab-al <asxabal7@gmail.com>
2025-09-02 21:57:18 +0300

    fix/finished endpoint categories list

--- app/api/categories/list.py
+++ app/api/categories/list.py
@@ -2,6 +2,8 @@ from fastapi import Depends, APIRouter, Body, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_async_session
 from typing import Annotated
+from services.api.category.categories_list import categories_list
+from schemas.api.categories.list.response import ListCategoryResponse
 
 
 router = APIRouter()
@@ -9,7 +11,12 @@ router = APIRouter()
 @router.get(
     "/",
     summary='Получить все категории',
-    description='Возвращает все категории которые есть'
+    description='Возвращает все категории которые есть',
+    response_model=ListCategoryResponse
 )
-async def list_categories():
-    ...
+async def list_categories(
+    session: Annotated[AsyncSession, Depends(get_async_session)]
+):
+    categories = await categories_list(session)
+
+    return ListCategoryResponse(categories=categories)
