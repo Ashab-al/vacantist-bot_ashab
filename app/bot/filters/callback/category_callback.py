687be8217bd16f56c2867acb916fad3fbb0075f9
ashab-al <asxabal7@gmail.com>
2025-10-03 16:07:16 +0300

    fix/doc update_subscription_with_category and create enum CategorySubscriptionEnum

--- app/bot/filters/callback/category_callback.py
+++ app/bot/filters/callback/category_callback.py
@@ -1,4 +1,12 @@
 from aiogram.filters.callback_data import CallbackData
 
+
 class CategoryCallback(CallbackData, prefix="category"):
+    """
+    Callback-данные для inline-кнопок категорий.
+
+    Используется для передачи `category_id` при нажатии на кнопку,
+    чтобы бот мог определить, к какой категории относится действие
+    (подписка или отписка).
+    """
     category_id: int
---
+++ app/enums/category_subscription_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class CategorySubscriptionEnum(Enum):
+    SUBSCRIBE = 'subscribe'
+    UNSUBSCRIBE = 'unsubscribe'
+    
