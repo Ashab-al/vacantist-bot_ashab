4e6184dd3d8fab720f1482e869c324afa60765b7
ashab-al <asxabal7@gmail.com>
2025-10-03 18:23:22 +0300

    fix/doc send_info_about_new_payment

--- app/services/tg/send_info_about_new_payment.py
+++ app/services/tg/send_info_about_new_payment.py
@@ -9,7 +9,14 @@ async def send_info_about_new_payment(
     tariff_data: TariffCallback,
     aiogram_user: AiogramTgUser        
 ) -> None:
-    
+    """
+    Отправляет в группу администратора информацию о новом платеже пользователя.
+
+    Args:
+        bot (Bot): Экземпляр Telegram-бота для отправки сообщения.
+        tariff_data (TariffCallback): Данные о тарифе/платеже.
+        aiogram_user (AiogramTgUser): Пользователь Telegram, который совершил платёж.
+    """
     await bot.send_message(
         chat_id=settings.admin_chat_id,
         text=await jinja_render('payment_info_for_admin', {
