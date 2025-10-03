e2564c1524f2b459d2d58a6af8bd1b21e40b9bc7
ashab-al <asxabal7@gmail.com>
2025-10-03 15:32:27 +0300

    fix/doc update_points

--- app/services/tg/user/update_points.py
+++ app/services/tg/user/update_points.py
@@ -1,21 +1,32 @@
 from models.user import User
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from schemas.tg.user.tg_user import TgUser
 from repositories.users.get_user_by_platform_id import get_user_by_platform_id
 from aiogram.types.user import User as AiogramTgUser
-from repositories.users.get_user_by_platform_id import get_user_by_platform_id
 
 
 async def update_points(
     db: AsyncSession,     
     aiogram_user: AiogramTgUser,
     points: int 
-):
+) -> None:
+    """
+    Обновить количество поинтов (`point`) у пользователя
+
+    Args:
+        db (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных.
+        aiogram_user (AiogramTgUser): Объект пользователя телеграм
+        points (int): Количество поинтов на добавление к пользователю
+    
+    Returns:
+        None
+    
+    Raises:
+        ValueError: Пользователь с platform_id `user.platform_id` не найден
+    """
     user: User = await get_user_by_platform_id(db, aiogram_user.id)
     
     if user is None:
-        raise ValueError(f"Пользователь с platform_id {user_platform_id} не найден")
+        raise ValueError(f"Пользователь с platform_id {user.platform_id} не найден")
 
     user.point = user.point + points
 
