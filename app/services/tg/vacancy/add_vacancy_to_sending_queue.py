ef4781aa6f872b05f0bfbe7691dbedbaafed83ee
ashab-al <asxabal7@gmail.com>
2025-10-03 16:52:43 +0300

    fix/add_vacancy_to_sending_queue and sender_worker

--- app/services/tg/vacancy/add_vacancy_to_sending_queue.py
+++ app/services/tg/vacancy/add_vacancy_to_sending_queue.py
@@ -1,10 +1,15 @@
 from models.vacancy import Vacancy
-import asyncio
 from config import vacancy_queue
 
 
 async def add_vacancy_to_sending_queue(
     vacancy: Vacancy
 ) -> None:
+    """
+    Добавляет вакансию в очередь на рассылку
+
+    Args:
+        vacancy (Vacancy): Объект вакансии, который нужно поставить в очередь
+    """
     print(f"Добавляю вакансию '{vacancy.title}' в очередь на рассылку.")
     await vacancy_queue.put(vacancy)
--- app/services/tg/vacancy/sender_worker.py
+++ app/services/tg/vacancy/sender_worker.py
@@ -8,22 +8,47 @@ from lib.tg.common import jinja_render
 from aiogram.exceptions import TelegramForbiddenError
 import asyncio
 from database import get_async_session_for_bot
+from enums.bot_status_enum import BotStatusEnum
+from config import settings
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-BASE_DELAY = 0.1  # минимальная задержка
-MAX_DELAY = 1.0   # максимальная задержка
 DELAY = 0.4
 async def sender_worker(
     queue: asyncio.Queue,
     bot: Bot
-):
+) -> None:
+    """
+    Асинхронный воркер для рассылки вакансий подписанным пользователям.
+
+    Работает в бесконечном цикле: получает вакансии из очереди и рассылает их
+    пользователям, подписанным на соответствующую категорию. Между отправками
+    сообщений делается пауза в `DELAY` секунд. Завершается при получении `None`
+    из очереди.
+
+    Args:
+        queue (asyncio.Queue): Асинхронная очередь FIFO, в которую добавляются
+            объекты `Vacancy` или `None` для остановки воркера.
+        bot (Bot): Экземпляр Telegram-бота, используемый для отправки сообщений.
+
+    Raises:
+        TelegramForbiddenError: Если бот не имеет права отправлять сообщения пользователю.
+        Exception: При других неожиданных ошибках.
+
+    Notes:
+        - Если пользователь заблокировал бота, его статус обновляется в базе данных
+          на `BOT_BLOCKED`.
+        - Для остановки воркера в очередь нужно положить `None`.
+    """
     print("Воркер для рассылки запущен и ждет вакансии...")
+    await admin_alert(bot, "Воркер для рассылки запущен и ждет вакансии...")
     while True:
         print("ЦИКЛ ЗАПУЩЕН, ОЖИДАЮ ВАКАНСИИ")
         vacancy: Vacancy | None = await queue.get()
         
         if vacancy is None:
             print("Получен сигнал на завершение. Воркер останавливается.")
+            await admin_alert(bot, "Получен сигнал на завершение. Воркер останавливается.")
             queue.task_done()
             break
         print(f"Начинаю рассылку вакансии: {vacancy.title}")
@@ -33,7 +58,7 @@ async def sender_worker(
             blocked_user_ids: list[int] = []
             if not users:
                 print(f"Для вакансии '{vacancy.title}' не найдено подписчиков.")
-                
+                await admin_alert(bot, f"Для вакансии '{vacancy.title}' не найдено подписчиков.")
                 queue.task_done()
                 continue 
 
@@ -41,30 +66,61 @@ async def sender_worker(
             
             for user in users:
                 try:
-                    res = await bot.send_message(
+                    await bot.send_message(
                         chat_id=user.platform_id,
                         text=await jinja_render('vacancy', { "vacancy": vacancy, "user": user }),
                         reply_markup=await vacancy_keyboard(vacancy=vacancy, user=user)
                     )
-                    print("ВАКАНСИЯ ОТПРАВЛЕНА")
-                    print(res)
                 except TelegramForbiddenError:
                     blocked_user_ids.append(user.id)
-                # TODO except Exceptions
+                except Exception as e:
+                    await admin_alert(bot, str(e) + '\n' + '\n' + 'sender_worker')
+                
                 await asyncio.sleep(delay)
             
             if blocked_user_ids:
-                await db.execute(
-                    (
-                        update(User)
-                        .where(User.id.in_(blocked_user_ids))
-                        .values(bot_status=User.BOT_STATUS_BLOCKED)
-                    )
-                )
-                await db.commit()
-                print(f"Обновлен статус для {len(blocked_user_ids)} заблокированных пользователей.")
+                await update_users_bot_status(db, blocked_user_ids)
+                await admin_alert(bot, f"Обновлен статус для {len(blocked_user_ids)} заблокированных пользователей.")
 
         queue.task_done()
         print(f"Рассылка вакансии '{vacancy.title}' завершена.")
+        await admin_alert(bot, f"Рассылка вакансии '{vacancy.title}' завершена.")
     
     print("ВЫХОД ИЗ ЦИКЛА")
+    await admin_alert(bot, "ВЫХОД ИЗ ЦИКЛА")
+
+async def update_users_bot_status(
+    db: AsyncSession, 
+    blocked_user_ids: list[int]
+) -> None:
+    """
+    Обновить `bot_status` на `BOT_BLOCKED` у пользователей
+
+    Args:
+        db (AsyncSession): app/services/tg/user/update_points.py
+        blocked_user_ids (list[int]): Список id пользователей
+    """
+    await db.execute(
+        (
+            update(User)
+            .where(User.id.in_(blocked_user_ids))
+            .values(bot_status=BotStatusEnum.BOT_BLOCKED)
+        )
+    )
+    await db.commit()
+
+async def admin_alert(
+    bot: Bot, 
+    text: str
+) -> None:
+    """
+    Отправить в группу админа уведомление
+
+    Args:
+        bot (Bot): Экземпляр бота, для отправки сообщения
+        text (str): Текст, который нужно отправить в группу
+    """
+    await bot.send_message(
+        chat_id=settings.admin_chat_id,
+        text=text
+    )
