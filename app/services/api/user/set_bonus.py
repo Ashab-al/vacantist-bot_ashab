8c5ab3c8516623fbd9d24e5897527fef0707d135
ashab-al <asxabal7@gmail.com>
2025-10-04 10:33:02 +0300

    fix/doc set_bonus

--- app/services/api/user/set_bonus.py
+++ app/services/api/user/set_bonus.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from schemas.api.users.set_bonus.request import SetBonusUserIdRequest, SetBonusRequest
-from sqlalchemy import select
 from models.user import User
 from services.api.user.find_user_by_id import find_user_by_id
 
@@ -10,6 +9,17 @@ async def set_bonus(
     user_id: SetBonusUserIdRequest,
     bonus: SetBonusRequest
 ) -> User:
+    """
+    Обновляет количество бонусов у пользователя
+
+    Args:
+        db (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных
+        user_id (SetBonusUserIdRequest): Схема для поиска пользователя
+        bonus (SetBonusRequest): Схема для обновления количество бонусов у пользователя
+    
+    Returns:
+        User: Объект пользователя
+    """
     user: User = await find_user_by_id(db, user_id)
     
     user.bonus = bonus.count
