86664a88c31aa2cd0c2b3caddce2ff54660c4985
ashab-al <asxabal7@gmail.com>
2025-10-04 19:12:59 +0300

    fix/doc /bot/filters/callback/*

--- app/bot/filters/callback/get_vacancies_callback.py
+++ app/bot/filters/callback/get_vacancies_callback.py
@@ -1,5 +1,16 @@
 from aiogram.filters.callback_data import CallbackData
 
 class GetVacanciesCallback(CallbackData, prefix="get_vacancies"):
+    """
+    Callback-данные для пагинации списка вакансий.
+
+    Attributes:
+        page (int): Номер страницы, на которой находится пользователь.
+        page_size (int): Количество вакансий на одной странице.
+
+    Этот callback используется для управления навигацией по списку вакансий
+    в inline-кнопках. Значения `page` и `page_size` помогают боту
+    определить, какие вакансии показать пользователю.
+    """
     page: int
     page_size: int
