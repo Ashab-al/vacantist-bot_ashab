5d3dabe14ae91bd5aeb8bf13b3fa1272f9bae68d
ashab-al <asxabal7@gmail.com>
2025-10-04 10:24:43 +0300

    fix/doc find_user_by_id

--- app/services/api/user/find_user_by_id.py
+++ app/services/api/user/find_user_by_id.py
@@ -1,8 +1,6 @@
 from schemas.api.users.show.request import ShowUserRequest
 from models.user import User
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
-from sqlalchemy import select
 from repositories.users.get_user_by_id import get_user_by_id
 
 
@@ -10,6 +8,19 @@ async def find_user_by_id(
     db: AsyncSession,
     user_id: ShowUserRequest
 ) -> User:
+    """
+    Возвращает пользователя по `id`
+
+    Args:
+        db (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных
+        user_id (ShowUserRequest): Схема для поиска пользователя
+    
+    Returns:
+        User: Объект пользователя
+    
+    Raises:
+        ValueError: Пользователя по `id` - `user_id.id` нет в базе
+    """
     user: User | None = await get_user_by_id(
         db,
         user_id.id
