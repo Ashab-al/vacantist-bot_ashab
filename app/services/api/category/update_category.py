f447122017fadccc338c337395e9cbf6b5ae547c
ashab-al <asxabal7@gmail.com>
2025-10-04 10:21:30 +0300

    fix/doc update_category

--- app/services/api/category/update_category.py
+++ app/services/api/category/update_category.py
@@ -2,11 +2,26 @@ from models.category import Category
 from sqlalchemy.ext.asyncio import AsyncSession
 from schemas.api.categories.update.request import UpdateCategoryRequest
 
+
 async def update_category(
     db: AsyncSession,
     category_id: int,
     category_data: UpdateCategoryRequest
 ) -> Category:
+    """
+    Обновляет категорию по `id` категории
+
+    Args:
+        db (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных
+        category_id (int): `id` категории
+        category_data (UpdateCategoryRequest): Схема для обновления категории
+    
+    Returns:
+        Category: Обновленная категория
+    
+    Raises:
+        ValueError: Категории по id - `category_id` нет в базе
+    """
     category: Category = await db.get(Category, category_id)
 
     if not category:
