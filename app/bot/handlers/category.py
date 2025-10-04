2c1845d91b0292316bab6676f3a09368eb2dd7ad
ashab-al <asxabal7@gmail.com>
2025-10-04 19:19:51 +0300

    fix/doc handlers/category

--- app/bot/handlers/category.py
+++ app/bot/handlers/category.py
@@ -4,38 +4,73 @@ from lib.tg.common import jinja_render
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import with_session
 from bot.keyboards.with_all_categories_keyboard import with_all_categories_keyboard
-from bot.filters.button import (
-    CategoryButtonFilter,
-)
+from bot.filters.button import CategoryButtonFilter
 from services.tg.category.find_subscribe import find_subscribe
 from bot.filters.callback.category_callback import CategoryCallback
 from services.tg.user.update_subscription_with_category import update_subscription_with_category
 from services.tg.user.current_user import current_user
 
 
-router = Router(name="Обработчик категорий")
-router.message.filter(F.chat.type == "private")
+router = Router(
+    name="Обработчик категорий"
+)
+router.message.filter(
+    F.chat.type == "private"
+)
 
-@router.message(CategoryButtonFilter())
+@router.message(
+    CategoryButtonFilter()
+)
 @with_session
 async def reaction_btn_categories(
     message: Message, 
     session: AsyncSession
 ) -> None:
-    subscribed_categories = await find_subscribe(session, message.from_user)
+    """
+    Обрабатывает нажатие на кнопку категорий в главном меню.
+
+    Args:
+        message (Message): Объект сообщения от пользователя.
+        session (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных.
+
+    Notes:
+        - Получает список категорий в виде inline кнопок.
+        - Отправляет сообщение с клавиатурой, где пользователь может выбрать или изменить подписки на категории.
+    """
+    subscribed_categories = await find_subscribe(
+        session, 
+        message.from_user
+    )
     await message.answer(
         await jinja_render('choice_category'), 
-        reply_markup=await with_all_categories_keyboard(session, subscribed_categories)
+        reply_markup=await with_all_categories_keyboard(
+            session, 
+            subscribed_categories
+        )
     )
 
-@router.callback_query(CategoryCallback.filter())
+@router.callback_query(
+    CategoryCallback.filter()
+)
 @with_session
 async def reaction_btn_choice_category(
     query: CallbackQuery, 
     callback_data: CategoryCallback, 
     session: AsyncSession
 ) -> None:
-    """Реакция на кнопки с категориями"""
+    """
+    Обрабатывает нажатие на inline-кнопки категорий.
+
+    Args:
+        query (CallbackQuery): CallbackQuery от пользователя.
+        callback_data (CategoryCallback): Данные callback для выбранной категории.
+        session (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных.
+
+    Notes:
+        - Обновляет подписку пользователя на выбранную категорию через `update_subscription_with_category`.
+        - Обновляет клавиатуру сообщений с категориями.
+        - Отправляет пользователю уведомление (show_alert) с результатом действия.
+    """
     view_path: dict[str, str] = await update_subscription_with_category(
         callback_data, 
         session, 
@@ -43,16 +78,24 @@ async def reaction_btn_choice_category(
             session, 
             query.from_user
         ), 
-        await current_user(session, query=query)
+        await current_user(
+            session, 
+            query=query
+        )
     )
     await query.message.edit_reply_markup(
         reply_markup=await with_all_categories_keyboard(
             session, 
-            await find_subscribe(session, query.from_user)
+            await find_subscribe(
+                session, 
+                query.from_user
+            )
         )
     )
 
     await query.answer(
-        await jinja_render(f"callback_query/{view_path['path_to_templates']}"),
+        await jinja_render(
+            f"callback_query/{view_path['path_to_templates']}"
+        ),
         show_alert=True
     )
