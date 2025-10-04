b72f81675d482bbf6cf1fb4f04eaf9cf8104b5dd
ashab-al <asxabal7@gmail.com>
2025-10-04 14:19:05 +0300

    fix/doc get_category_by_id

--- app/repositories/categories/get_all_categories.py
+++ app/repositories/categories/get_all_categories.py
@@ -3,13 +3,16 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_all_categories(
-    db: AsyncSession
-) -> list[Category]:
-    categories: list[Category] = (
-        await db.execute(
-            select(Category)
-        )
-    ).scalars().all()
+async def get_all_categories(db: AsyncSession) -> list[Category]:
+    """
+    Получить список всех категорий из базы данных.
 
+    Args:
+        db (AsyncSession): Асинхронная сессия SQLAlchemy для работы с базой данных.
+
+    Returns:
+        list[Category]: Список всех категорий.
+    """
+    result = await db.execute(select(Category))
+    categories = result.scalars().all()
     return categories
