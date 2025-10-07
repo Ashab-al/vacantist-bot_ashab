7b7c58cd40d7975c62fb9e33add05186d3f4a6a7
ashab-al <asxabal7@gmail.com>
2025-10-07 11:33:08 +0300

    fix/test test_find_user_by_id and create fixture new_tg_user

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -11,6 +11,10 @@ from sqlalchemy.ext.asyncio import (  # noqa: E501
     create_async_engine,
 )
 
+from schemas.tg.user.tg_user import TgUser
+from models.user import User
+import random
+
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 from models.base import Base  # noqa: E402
 
@@ -70,3 +74,32 @@ async def session_factory() -> AsyncGenerator[async_sessionmaker[AsyncSession], 
     factory = async_sessionmaker(engine, expire_on_commit=False)
     yield factory
     await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def new_tg_user(
+    session_factory
+) -> User:
+    """Возвращает нового пользователя для тестов"""
+    user_data: dict[str, str | int] = {
+        "id": random.randint(1000, 100000000),
+        "first_name": f"Имя {random.randint(1, 1000)}",
+        "username": f"asd{random.randint(1, 1000)}"
+    }
+    new_user_schema: TgUser = TgUser.model_validate(user_data)
+    async with session_factory() as session:
+        user: User = User(
+            platform_id=new_user_schema.id,
+            first_name=new_user_schema.first_name,
+            username=new_user_schema.username,
+            email=new_user_schema.email,
+            phone=new_user_schema.phone,
+            point=new_user_schema.point,
+            bonus=new_user_schema.bonus,
+            bot_status=new_user_schema.bot_status
+        )
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    return user
---
+++ app/tests/services/api/user/find_user_by_id_test.py
@@ -0,0 +1,24 @@
+import pytest
+from models.user import User
+from services.api.user.find_user_by_id import find_user_by_id
+import random
+from schemas.api.users.show.request import ShowUserRequest
+
+
+@pytest.mark.asyncio
+async def test_find_user_by_id(
+    session_factory,
+    new_tg_user: User
+):
+    """Проверяет поиск категории по id"""
+    
+    async with session_factory() as session:
+        user: User = await find_user_by_id(
+            session, 
+            ShowUserRequest(id=new_tg_user.id)
+        )
+    
+    assert isinstance(user, User)
+    assert user.id == new_tg_user.id
+    assert user.platform_id == new_tg_user.platform_id
+    assert user.first_name == new_tg_user.first_name
