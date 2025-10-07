d88eb9d8a5b3b0735477b79ca49046d7a9d150d9
ashab-al <asxabal7@gmail.com>
2025-10-07 13:35:09 +0300

    fix/test set_status_test

---
+++ app/tests/services/api/user/set_status_test.py
@@ -0,0 +1,47 @@
+import pytest
+from models.user import User
+from services.api.user.set_status import set_status
+import random
+from schemas.api.users.set_status.request import SetStatusUserIdRequest, SetStatusRequest
+from enums.bot_status_enum import BotStatusEnum
+
+
+@pytest.mark.asyncio
+async def test_set_status(
+    session_factory,
+    new_tg_user: User
+):
+    """Проверяет обновление статуса у пользователя"""
+
+    async with session_factory() as session:
+        user: User = await set_status(
+            session,
+            SetStatusUserIdRequest(id=new_tg_user.id),
+            SetStatusRequest(bot_status=BotStatusEnum.BOT_BLOCKED)
+        )
+    
+    assert isinstance(user, User)
+    assert user.bot_status == BotStatusEnum.BOT_BLOCKED
+    assert user.id == new_tg_user.id
+    assert user.platform_id == new_tg_user.platform_id
+    assert user.first_name == new_tg_user.first_name
+
+
+@pytest.mark.asyncio
+async def test_set_status_when_user_not_exist(
+    session_factory
+):
+    """Проверяет обновление статуса у не существующего пользователя"""
+    user_id: int = random.randint(1, 100)
+
+    with pytest.raises(
+        ValueError, 
+        match=f"Пользователя по id - {user_id} нет в базе"
+    ):
+        async with session_factory() as session:
+            await set_status(
+                session,
+                SetStatusUserIdRequest(id=user_id),
+                SetStatusRequest(bot_status=BotStatusEnum.BOT_BLOCKED)
+            )
+    
