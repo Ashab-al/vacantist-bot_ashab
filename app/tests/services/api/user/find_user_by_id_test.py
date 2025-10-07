740b81a646cd58926152b90998a08f3d60bdeb96
ashab-al <asxabal7@gmail.com>
2025-10-07 12:07:07 +0300

    fix/add test test_find_user_by_id_when_user_not_exist

--- app/tests/services/api/user/find_user_by_id_test.py
+++ app/tests/services/api/user/find_user_by_id_test.py
@@ -10,7 +10,7 @@ async def test_find_user_by_id(
     session_factory,
     new_tg_user: User
 ):
-    """Проверяет поиск категории по id"""
+    """Проверяет поиск пользователя по id"""
     
     async with session_factory() as session:
         user: User = await find_user_by_id(
@@ -22,3 +22,21 @@ async def test_find_user_by_id(
     assert user.id == new_tg_user.id
     assert user.platform_id == new_tg_user.platform_id
     assert user.first_name == new_tg_user.first_name
+
+@pytest.mark.asyncio
+async def test_find_user_by_id_when_user_not_exist(
+    session_factory
+):
+    """Проверяет поиск не существующего пользователя по id"""
+    user_id: int = random.randint(1, 100)
+    
+    with pytest.raises(
+        ValueError, 
+        match=f"Пользователя по id - {user_id} нет в базе"
+    ):
+        async with session_factory() as session:
+            await find_user_by_id(
+                session,
+                ShowUserRequest(id=user_id)
+            )
+    
