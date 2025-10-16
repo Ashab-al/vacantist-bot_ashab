ff78c12eaa1133c92ed7b579af86d13361872e49
ashab-al <asxabal7@gmail.com>
2025-10-16 19:55:47 +0300

    fix/add index for table subscriptions

--- app/models/subscription.py
+++ app/models/subscription.py
@@ -1,11 +1,14 @@
 from models.base import Base
-from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy import Column, ForeignKey, Index, Table
 
 subscription = Table(
     "subscriptions",
     Base.metadata,
     Column("user_id", ForeignKey("users.id"), primary_key=True),
     Column("category_id", ForeignKey("categories.id"), primary_key=True),
+    Index("idx_subscriptions_category_id", "category_id"),
+    Index("idx_subscriptions_user_id", "user_id")
+
 )
 """
 Промежуточная таблица для связи многие-ко-многим между пользователями и категориями.
