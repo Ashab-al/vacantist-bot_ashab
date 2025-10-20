5a40624b6de72f689032710eeec78b04b70f3179
ashab-al <asxabal7@gmail.com>
2025-10-21 00:13:10 +0300

    fix/open_vacancy_test

--- app/tests/services/tg/advertisement_test.py
+++ app/tests/services/tg/advertisement_test.py
@@ -1,25 +1,22 @@
 import random
+from unittest.mock import AsyncMock, Mock
 
 import pytest
 from models.category import Category
 from services.tg.advertisement import advertisement
 from tests.factories.category import CategoryFactory
-from unittest.mock import AsyncMock, Mock
+
 
 @pytest.mark.asyncio
 async def test_advertisement():
     """Тестирует получение списка категорий с количеством пользователей"""
     one_user: int = 1
     subscribe_count: int = random.randint(3, 10)
-    categories: list[Category] = [
-        CategoryFactory() for _ in range(subscribe_count)
-    ]
+    categories: list[Category] = [CategoryFactory() for _ in range(subscribe_count)]
 
     mock_db = AsyncMock()
     mock_all = Mock()
-    mock_all.all.return_value = [
-        (category.name, one_user) for category in categories
-    ]
+    mock_all.all.return_value = [(category.name, one_user) for category in categories]
     mock_db.execute.return_value = mock_all
 
     result: list[tuple[str, int]] = await advertisement(mock_db)
--- app/tests/services/tg/open_vacancy_test.py
+++ app/tests/services/tg/open_vacancy_test.py
@@ -1,75 +1,84 @@
 import random
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from enums.check_vacancy_enum import CheckVacancyEnum
-from models.blacklist import BlackList
 from models.user import User
-from services.tg.vacancy.open_vacancy import ZERO_BALANCE, open_vacancy
-from services.tg.vacancy.spam_vacancy import COMPLAINT_COUNTER
-from tests.conftest import (
-    create_tg_user_with_session,
-    create_vacancy_and_category_with_session,
-)
+from services.tg.vacancy.open_vacancy import open_vacancy
+from tests.factories.user import UserFactoryWithoutSubscriptions
+from tests.factories.vacancy import VacancyWithCategoryFactory
 
 
 @pytest.mark.asyncio
-async def test_open_vacancy(session):
+@patch("services.tg.vacancy.open_vacancy._check_blacklist")
+@patch("services.tg.vacancy.open_vacancy.find_vacancy_by_id")
+async def test_open_vacancy(mock_find_vacancy_by_id, mock_check_blacklist):
     """Проверяет открытие вакансии"""
-    vacancy, _category = await create_vacancy_and_category_with_session(session)
-    user: User = await create_tg_user_with_session(session)
-    result_open_vacancy: dict = await open_vacancy(session, user, vacancy.id)
+    user_point_count: int = 3
+    vacancy = VacancyWithCategoryFactory()
+    user: User = UserFactoryWithoutSubscriptions()
+    user.point = user_point_count
+    mock_db = AsyncMock()
+    mock_find_vacancy_by_id.return_value = vacancy
+    mock_check_blacklist.return_value = False
+
+    result_open_vacancy: dict = await open_vacancy(mock_db, user, vacancy.id)
 
     assert result_open_vacancy.get("status") == CheckVacancyEnum.OPEN_VACANCY
     assert result_open_vacancy.get("vacancy") == vacancy
     assert result_open_vacancy.get("path_view") in "callback_query/open_vacancy"
-    assert result_open_vacancy.get("low_points") == True
+    assert result_open_vacancy.get("low_points") is True
 
 
 @pytest.mark.asyncio
-async def test_open_vacancy_when_vacancy_is_not_exist(session):
+@patch("services.tg.vacancy.open_vacancy.find_vacancy_by_id")
+async def test_open_vacancy_when_vacancy_is_not_exist(mock_find_vacancy_by_id):
     """Проверяет открытие вакансии когда вакансии не существует"""
+    mock_find_vacancy_by_id.return_value = None
     vacancy_id: int = random.randint(1, 1000)
-
+    mock_db = AsyncMock()
+    user: User = UserFactoryWithoutSubscriptions()
     with pytest.raises(
         ValueError, match=f"{vacancy_id} - такой вакансии нет в базе данных"
     ):
-        user: User = await create_tg_user_with_session(session)
-
-        await open_vacancy(session, user, vacancy_id)
+        await open_vacancy(mock_db, user, vacancy_id)
 
 
 @pytest.mark.asyncio
-async def test_open_vacancy_when_user_balance_zero(session):
+@patch("services.tg.vacancy.open_vacancy._check_blacklist")
+@patch("services.tg.vacancy.open_vacancy.find_vacancy_by_id")
+async def test_open_vacancy_when_user_balance_zero(
+    mock_find_vacancy_by_id, mock_check_blacklist
+):
     """Проверяет открытие вакансии когда баланс пользователя нулевой"""
-    vacancy, _category = await create_vacancy_and_category_with_session(session)
-    user: User = await create_tg_user_with_session(session)
-    user.bonus = ZERO_BALANCE
-    user.point = ZERO_BALANCE
+    vacancy = VacancyWithCategoryFactory()
+    user: User = UserFactoryWithoutSubscriptions()
+    mock_db = AsyncMock()
+    mock_find_vacancy_by_id.return_value = vacancy
+    mock_check_blacklist.return_value = False
 
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
-
-    result_open_vacancy: dict = await open_vacancy(session, user, vacancy.id)
+    result_open_vacancy: dict = await open_vacancy(mock_db, user, vacancy.id)
 
     assert result_open_vacancy.get("status") == CheckVacancyEnum.WARNING
     assert result_open_vacancy.get("path_view") in "callback_query/out_of_points"
 
 
 @pytest.mark.asyncio
-async def test_open_vacancy_when_vacancy_in_blacklist(session):
+@patch("services.tg.vacancy.open_vacancy._check_blacklist")
+@patch("services.tg.vacancy.open_vacancy.find_vacancy_by_id")
+async def test_open_vacancy_when_vacancy_in_blacklist(
+    mock_find_vacancy_by_id, mock_check_blacklist
+):
     """Проверяет открытие вакансии когда вакансия находится в черном списке"""
-    vacancy, _category = await create_vacancy_and_category_with_session(session)
-    user: User = await create_tg_user_with_session(session)
+    user_point_count: int = 3
+    vacancy = VacancyWithCategoryFactory()
+    user: User = UserFactoryWithoutSubscriptions()
+    user.point = user_point_count
+    mock_db = AsyncMock()
+    mock_find_vacancy_by_id.return_value = vacancy
+    mock_check_blacklist.return_value = True
 
-    blacklisted = BlackList(
-        contact_information=vacancy.platform_id, complaint_counter=COMPLAINT_COUNTER
-    )
-
-    session.add(blacklisted)
-    await session.commit()
-
-    result_open_vacancy: dict = await open_vacancy(session, user, vacancy.id)
+    result_open_vacancy: dict = await open_vacancy(mock_db, user, vacancy.id)
     assert result_open_vacancy.get("status") == CheckVacancyEnum.WARNING
     assert result_open_vacancy.get("vacancy") == vacancy
     assert result_open_vacancy.get("path_view") in "callback_query/add_to_blacklist"
