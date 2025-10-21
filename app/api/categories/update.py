5956d44406cec909de68f95d95071d5253dbb9da
ashab-al <asxabal7@gmail.com>
2025-10-21 22:38:14 +0300

    fix/update_category_test

--- app/api/categories/update.py
+++ app/api/categories/update.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from database import get_async_session
+from exceptions.category.category_not_found_error import CategoryNotFoundError
 from fastapi import APIRouter, Body, Depends, HTTPException, Path
 from schemas.api.categories.update import UpdateCategoryRequest, UpdateCategoryResponse
 from services.api.category.update_category import update_category
@@ -41,7 +42,7 @@ async def update(
     """
     try:
         category = await update_category(session, category_id, category_data)
-    except ValueError as e:
+    except CategoryNotFoundError as e:
         raise HTTPException(status_code=404, detail=str(e)) from e
 
     return category
--- app/tests/requests/category/update_test.py
+++ app/tests/requests/category/update_test.py
@@ -36,9 +36,7 @@ async def test_update_category_when_category_is_not_exist(client):
     )
 
     assert response.status_code == 404
-    assert (
-        response.json().get("detail") == f"Категории по id - {category_id} нет в базе"
-    )
+
 
 
 @pytest.mark.asyncio
