a45c00a755712fcaf3d20d31d95ce33d4aa9acf9
ashab-al <asxabal7@gmail.com>
2025-10-22 11:12:32 +0300

    fix/test_set_status

--- app/tests/services/api/user/set_status_test.py
+++ app/tests/services/api/user/set_status_test.py
@@ -1,4 +1,5 @@
 import random
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from enums.bot_status_enum import BotStatusEnum
@@ -9,33 +10,41 @@ from schemas.api.users.set_status.request import (
     SetStatusUserIdRequest,
 )
 from services.api.user.set_status import set_status
+from tests.factories.user import UserFactoryWithoutSubscriptions
 
 
 @pytest.mark.asyncio
-async def test_set_status(session, new_tg_user: User):
+@patch("services.api.user.find_user_by_id.get_user_by_id")
+async def test_set_status(mock_get_user_by_id):
     """Проверяет обновление статуса у пользователя"""
+    new_user = UserFactoryWithoutSubscriptions()
+    mock_db = AsyncMock()
+    mock_get_user_by_id.return_value = new_user
 
     user: User = await set_status(
-        session,
-        SetStatusUserIdRequest(id=new_tg_user.id),
+        mock_db,
+        SetStatusUserIdRequest(id=new_user.id),
         SetStatusRequest(bot_status=BotStatusEnum.BOT_BLOCKED),
     )
 
+    mock_get_user_by_id.assert_awaited_once_with(mock_db, new_user.id)
     assert isinstance(user, User)
     assert user.bot_status == BotStatusEnum.BOT_BLOCKED
-    assert user.id == new_tg_user.id
-    assert user.platform_id == new_tg_user.platform_id
-    assert user.first_name == new_tg_user.first_name
+    assert user.id == new_user.id
+    assert user.platform_id == new_user.platform_id
+    assert user.first_name == new_user.first_name
 
 
 @pytest.mark.asyncio
-async def test_set_status_when_user_not_exist(session):
+@patch("services.api.user.find_user_by_id.get_user_by_id")
+async def test_set_status_when_user_not_exist(mock_get_user_by_id):
     """Проверяет обновление статуса у не существующего пользователя"""
     user_id: int = random.randint(1, 100)
-
+    mock_db = AsyncMock()
+    mock_get_user_by_id.return_value = None
     with pytest.raises(UserNotFoundError):
         await set_status(
-            session,
+            mock_db,
             SetStatusUserIdRequest(id=user_id),
             SetStatusRequest(bot_status=BotStatusEnum.BOT_BLOCKED),
         )
