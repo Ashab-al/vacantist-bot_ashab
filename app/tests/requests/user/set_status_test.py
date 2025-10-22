7e2ee46c2f2cafbd6d2816b674b2ea8fe44e2ea5
ashab-al <asxabal7@gmail.com>
2025-10-23 01:03:34 +0300

    fix/requests/user/set_status_test

--- app/tests/requests/user/set_status_test.py
+++ app/tests/requests/user/set_status_test.py
@@ -1,34 +1,36 @@
 import random
+from unittest.mock import patch
 
 import pytest
 from enums.bot_status_enum import BotStatusEnum
+from fastapi import status
 from models.user import User
-from tests.conftest import create_tg_user
+from tests.factories.user import UserFactoryWithoutSubscriptions
 
 
 @pytest.mark.asyncio
-async def test_set_status(client, session):
+@patch("services.api.user.find_user_by_id.get_user_by_id")
+async def test_set_status(mock_get_user_by_id, client):
     """Тестирует эндпоинт обновления статуса у пользователя"""
-    user: User = await create_tg_user(session)
-
+    user: User = UserFactoryWithoutSubscriptions()
+    mock_get_user_by_id.return_value = user
     response = await client.post(
         f"/users/{user.id}/set_status",
         json={"bot_status": BotStatusEnum.BOT_BLOCKED.value},
     )
 
-    assert response.status_code == 200
-    assert response.json().get("botStatus") == BotStatusEnum.BOT_BLOCKED.value
+    assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.asyncio
-async def test_set_status_when_user_is_not_exist(client):
+@patch("services.api.user.find_user_by_id.get_user_by_id")
+async def test_set_status_when_user_is_not_exist(mock_get_user_by_id, client):
     """Тестирует эндпоинт обновления статуса у пользователя, которого не существует"""
     user_id: int = random.randint(1, 100)
-
+    mock_get_user_by_id.return_value = None
     response = await client.post(
         f"/users/{user_id}/set_status",
         json={"bot_status": BotStatusEnum.BOT_BLOCKED.value},
     )
 
-    assert response.status_code == 404
-    # assert response.json().get("detail") == f"Пользователя по id - {user_id} нет в базе"
+    assert response.status_code == status.HTTP_404_NOT_FOUND
