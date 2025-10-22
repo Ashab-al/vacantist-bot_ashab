22b66f2f60ab5810aee5c45d9e9491704541a7c0
ashab-al <asxabal7@gmail.com>
2025-10-22 11:16:25 +0300

    fix/users_list_test

--- app/tests/services/api/user/users_list_test.py
+++ app/tests/services/api/user/users_list_test.py
@@ -1,20 +1,22 @@
 import random
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from models.user import User
 from services.api.user.users_list import users_list
-from tests.conftest import create_tg_user
+from tests.factories.user import UserFactoryWithoutSubscriptions
 
 
 @pytest.mark.asyncio
-async def test_users_list(session):
+@patch("services.api.user.users_list.get_all_users")
+async def test_users_list(mock_get_all_users):
     """Проверяет получение списка пользователей"""
     user_count = random.randint(3, 10)
+    mock_db = AsyncMock()
+    users: list[User] = [UserFactoryWithoutSubscriptions() for _ in range(user_count)]
+    mock_get_all_users.return_value = users
+    user_list: list[User] = await users_list(mock_db)
 
-    for _ in range(user_count):
-        await create_tg_user(session)
-
-    user_list: list[User] = await users_list(session)
-
+    mock_get_all_users.assert_awaited_once_with(mock_db)
     assert len(user_list) == user_count
     assert all(isinstance(user, User) for user in user_list)
