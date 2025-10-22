b2125a615fc814d6b6bf537e139e20378f76aba7
ashab-al <asxabal7@gmail.com>
2025-10-23 00:39:26 +0300

    fix/requests/category/list_test

--- app/tests/requests/category/list_test.py
+++ app/tests/requests/category/list_test.py
@@ -1,27 +1,25 @@
 import random
+from unittest.mock import patch
 
 import pytest
 from models.category import Category
-
+from tests.factories.category import CategoryFactory
+from fastapi import status
 
 @pytest.mark.asyncio
-async def test_list_categories(client, session):
+@patch("api.categories.list.ListCategoryResponse")
+@patch("services.api.category.categories_list.get_all_categories")
+async def test_list_categories(
+    mock_get_all_categories, mock_list_category_response, client
+):
     """Тестирует эндпоинт возврата списка всех существующих категорий"""
     category_count = random.randint(4, 10)
 
-    category_names: str = {f"Category {i}" for i in range(category_count)}
-    categories: list[Category] = []
-    for name in category_names:
-        category = Category(name=name)
-        session.add(category)
-        await session.commit()
-        await session.refresh(category)
-        categories.append(category)
+    categories: list[Category] = [CategoryFactory() for _ in range(category_count)]
 
+    mock_get_all_categories.return_value = categories
+    mock_list_category_response.return_value = {"categories": categories}
     response = await client.get("/categories/")
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert len(response.json().get("categories")) == len(categories)
-    assert {
-        category.get("name") for category in response.json().get("categories")
-    } == category_names
