4f80150b8afe9b60567feb9d33ccb22c3ea31f2b
ashab-al <asxabal7@gmail.com>
2026-02-07 04:59:57 +0300

    fix/edit locales and send_spam_vacancy_in_admin_group

--- app/services/tg/vacancy/send_spam_vacancy_in_admin_group.py
+++ app/services/tg/vacancy/send_spam_vacancy_in_admin_group.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.vacancy import Vacancy
+from models.user import User
 
 from bot.filters.callback.spam_vacancy_callback import SpamVacancyCallback, SpamVacancyCallbackForAdmin
 from aiogram import Bot
@@ -10,7 +11,9 @@ from bot.keyboards.admin_chat_spam_vacancy_button import admin_chat_spam_vacancy
 from config import settings
 from query_objects.vacancies.find_vacancy_by_id import find_vacancy_by_id
 from services.tg.admin_alert import admin_alert_mailing_errors
-
+from lib.tg.common import jinja_render
+from config import i18n
+from query_objects.users.get_user_by_platform_id import get_user_by_platform_id
 
 async def send_spam_vacancy_in_admin_group(
     bot: Bot,
@@ -35,16 +38,31 @@ async def send_spam_vacancy_in_admin_group(
 
         raise ValueError(f"В send_spam_vacancy_in_admin_group не найдена вакансия ID: {callback_data.vacancy_id}")
 
+    user: User | None = await get_user_by_platform_id(session, callback.from_user.id)
+    if not user:
+        await admin_alert_mailing_errors(
+            bot,
+            f"В send_spam_vacancy_in_admin_group не найден пользователь с telegram_id: {callback.from_user.id}"
+        )
+
+        raise ValueError(f"В send_spam_vacancy_in_admin_group не найден пользователь с telegram_id: {callback.from_user.id}")
+
     btn = await admin_chat_spam_vacancy_button(spam_vacancy)
-    text = f"""
-        Пользователь @{callback.from_user.username} пометил вакансию как спам.
-        Вакансия ID: {callback_data.vacancy_id}
-    """
-    text += "\n----------------------\n" + vacancy.description
 
     await bot.send_message(
         chat_id=settings.admin_chat_id,
-        text=text,
+        text=await jinja_render(
+            "spam/vacancy", {
+                "username": username_replacement(callback.from_user.username),
+                "vacancy": vacancy,
+                "user": user
+            }
+        ),
         reply_markup=btn,
         message_thread_id=settings.mailing_new_spam_vacancies_thread_id
     )
+
+def username_replacement(username: str | None) -> str:
+    if username is not None:
+        return username
+    return f"<code>{i18n['user']['user_not_has_username']}</code>"
